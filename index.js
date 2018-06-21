c1965ba539322867aeaa2e8b948a27c729bb4c35
zackmore <zackmoredev@gmail.com>
2018-06-21 23:57:22 +0800

    change default port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const app = require('./app');
 
-const port = process.env.SERVER_PORT || 3000;
+const port = process.env.SERVER_PORT || 5000;
 
 app.listen(port, '0.0.0.0', () => {
   console.log(`Listening on Port ${port}`);
