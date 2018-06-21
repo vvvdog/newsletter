3ced41fe4ac8ac7411a897df16602245d589e44b
zackmore <zackmoredev@gmail.com>
2018-06-21 22:49:23 +0800

    getupdates instead of getme

--- scripts/getUpdates.js
+++ scripts/getUpdates.js
@@ -1,21 +1,22 @@
 require('dotenv').config();
 const r2 = require('r2');
 
-const url = `https://api.telegram.org/bot${process.env.TELEGRAM_BOT_TOKEN}/getMe`;
+const url = `https://api.telegram.org/bot${process.env.TELEGRAM_BOT_TOKEN}/getUpdates`;
 
 async function run() {
   console.log(url);
-  const res = await r2.get(url, {
-    headers: {
-      'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-      'Accept-Encoding': 'gzip, deflate, br',
-      'Accept-Language': 'en-US,en;q=0.9,ja;q=0.8,zh-CN;q=0.7,zh;q=0.6',
-      'Cache-Control': 'no-cache',
-      'Connection': 'keep-alive',
-      'Host': 'api.telegram.org',
-      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36'
-    }
-  }).json;
+  const res = await r2(url).json;
+  // const res = await r2.get(url, {
+  //   headers: {
+  //     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+  //     'Accept-Encoding': 'gzip, deflate, br',
+  //     'Accept-Language': 'en-US,en;q=0.9,ja;q=0.8,zh-CN;q=0.7,zh;q=0.6',
+  //     'Cache-Control': 'no-cache',
+  //     'Connection': 'keep-alive',
+  //     'Host': 'api.telegram.org',
+  //     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36'
+  //   }
+  // }).json;
   console.log(res);
 }
 
