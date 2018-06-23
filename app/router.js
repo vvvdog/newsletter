dc218eaa8c03bf1ba0024de5e21f8737fe67dfe1
zackmore <zackmoredev@gmail.com>
2018-06-23 23:24:57 +0800

    handle telegram webhook

--- app/router.js
+++ app/router.js
@@ -1,6 +1,8 @@
+const dayjs = require('dayjs');
 const Router = require('koa-router');
 const conn = require('../database/connection');
 const scripts = require('../scripts');
+const { parseTelegramUpdate, getOpenGraphData } = require('../utils');
 
 const router = new Router();
 
@@ -22,7 +24,25 @@ router.post('/api/_run_fetch', async (ctx, next) => {
 });
 
 router.post('/api/posts', async (ctx, next) => {
-  console.log(ctx.request.body);
+  const data = parseTelegramUpdate(ctx.request.body);
+
+  if (data) {
+    const opengraph = await getOpenGraphData(data.link);
+    if (opengraph) data.opengraph = opengraph;
+    data.created_at = dayjs().toISOString();
+
+    try {
+      const post = await conn('posts').insert(data);
+      ctx.body = post;
+    } catch (e) {
+      ctx.throw(500, 'server error');
+    }
+  } else {
+    ctx.stats = 201;
+    ctx.body = '';
+  }
+
+  next();
 });
 
 router.get('/api/posts', async (ctx, next) => {
